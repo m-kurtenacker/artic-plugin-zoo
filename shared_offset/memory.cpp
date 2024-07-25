be51b4b6816cae7ee81e9cdd51baa661a231dbdb
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2024-07-25 17:06:33 +0200

    Run test for shared_offset. Add get_max_offset plugin function.

--- shared_offset/memory.cpp
+++ shared_offset/memory.cpp
@@ -6,24 +6,69 @@
 
 using namespace thorin;
 
+const Def* max(World* world, const Def* a, const Def* b) {
+    const Def* condition = world->cmp_ge(a, b);
+    return world->select(condition, a, b);
+}
+
 extern "C" {
+
+const Def* get_max_offset (World* world, const App* app) {
+    assert(app->num_args() == 3);
+
+    //const Def* offset = app->arg(1);
+    const Def* body = app->arg(1);
+    const Def* target = app->arg(2);
+
+    const Def* with_shared_continuation = nullptr;
+    for (auto cont : world->copy_continuations()) {
+        if (cont->name() == "with_shared") {
+            with_shared_continuation = cont;
+            break;
+        }
+    }
+
+    if (!with_shared_continuation) {
+        auto offset_arg = target->as<Continuation>()->body()->arg(1);
+
+        Continuation* y = world->continuation(target->type()->op(1)->as<FnType>());
+        y->jump(body, {y->param(0), offset_arg, y->param(2)});
+        return y;
+    } else {
+        Scope body_scope = Scope(const_cast<Continuation*>(body->as<Continuation>()));
+
+        App* target_app = const_cast<App*>(target->as<Continuation>()->body());
+        auto offset_arg = target_app->arg(1);
+
+        for (auto use : with_shared_continuation->uses()) {
+            if (body_scope.contains(use)) {
+                if (auto child_app = use->isa<App>()) {
+                    offset_arg = max(world, world->arithop_add(child_app->op(2), child_app->op(3)), offset_arg);
+                }
+            }
+        }
+
+        if (offset_arg != target_app->arg(1)) {
+            target_app->unset_op(2);
+            target_app->set_op(2, offset_arg);
+        }
+
+        throw std::runtime_error("Not finished yet.");
+    }
+}
 
 const Def* with_shared (World* world, const App* app) {
     if (!world->is_pe_done())
         throw std::runtime_error("This can only be optimized once the rest of the PE is done.");
 
-    //app->dump();
-    //world->dump_scoped();
     assert(app->num_args() == 5);
-    //const Def* in_mem = app->arg(0); //Not relevant
     const Def* size = app->arg(1);
     const Def* offset = app->arg(2);
     const Def* body = app->arg(3);
     const Def* target = app->arg(4);
 
-    if (!body->isa<Continuation>()) {
+    if (!body->isa<Continuation>())
         throw std::runtime_error("Cannot do anything useful here.");
-    }
 
     auto new_child_offset = world->arithop_add(size, offset);
 
@@ -33,7 +78,6 @@ const Def* with_shared (World* world, const App* app) {
         if (body_scope.contains(use)) {
             if (auto child_app = use->isa<App>()) { //XXX: This is ugly as blody hell, I should find a better way to implement this.
                 auto child_app_nonconst = const_cast<App*>(child_app);
-                //child_app_nonconst->op(3)->dump();
                 child_app_nonconst->unset_op(3);
                 child_app_nonconst->set_op(3, new_child_offset);
             }
